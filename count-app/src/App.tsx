4bdfea691eb34924ae4e47695c5e01d7e8f174ce
Ivan Grekov <ivan_grekov@yahoo.com>
2023-04-16 15:10:57 +0300

    feat: add App component

--- count-app/src/App.tsx
+++ count-app/src/App.tsx
@@ -1,13 +1,27 @@
-import ExpenseItem from "./components/ExpenseItem";
-import { expenses } from "./static/data";
+import React from "react";
+import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense";
+import { DUMMY_EXPENSES } from "./static/data";
+
+export interface INewExpense {
+  title: string;
+  amount: number;
+  date: Date;
+  id: string;
+}
 
 function App() {
+  const [expenses, setExpenses] = React.useState(DUMMY_EXPENSES);
+
+  const addExpensesHandler = (newExpense: INewExpense) => {
+    setExpenses((prevExpenses) => [newExpense, ...prevExpenses]);
+  };
+
   return (
-    <div>
-      {expenses.map((exp) => (
-        <ExpenseItem {...exp} key={exp.id} />
-      ))}
-    </div>
+    <>
+      <NewExpense onAddNewExpense={addExpensesHandler} />
+      <Expenses items={expenses} />
+    </>
   );
 }
 
