c5f88a1cccefec42de770c8f0e0814dbb7fd6396
Ivan Grekov <ivan_grekov@yahoo.com>
2023-04-22 22:40:51 +0300

    feat: add UsersList component

---
+++ users-table/src/components/Users/UsersList.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "../UI/Card";
+import styles from "./UserList.module.css";
+
+export interface IUser {
+  userName: string;
+  userAge: string;
+}
+
+const UsersList: React.FC<{ users: IUser[] }> = ({ users }) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {users.map((user, id) => (
+          <li key={id}>
+            {user.userName} ({user.userAge} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
