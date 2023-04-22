c70364608053dd21d6d24b219ff5065efa75e10b
Ivan Grekov <ivan_grekov@yahoo.com>
2023-04-22 22:03:49 +0300

    feat: add Card wrap component

---
+++ users-table/src/components/UI/Card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Card.module.css";
+
+const Card: React.FC<{ children: React.ReactNode; className: string }> = ({
+  children,
+  className,
+}) => {
+  const classes = `${styles.card} ${className}`;
+  return <div className={classes}>{children}</div>;
+};
+
+export default Card;
