1111e732705f09c87425edfc05c48fb1944aa6e6
Ivan Grekov <ivan_grekov@yahoo.com>
2023-04-24 12:31:20 +0300

    feat: add functionality to App file

--- project-hooks/src/App.tsx
+++ project-hooks/src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  return <div className="App"></div>;
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
+  const loginHandler = (email: string, password: string) => {
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
