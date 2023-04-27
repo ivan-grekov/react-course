8e8f116b7860f5bc73e1331906e6089be2b42e8d
Ivan Grekov <ivan_grekov@yahoo.com>
2023-04-27 14:45:59 +0300

    fix: delete unused props

--- project-hooks/src/components/Home/Home.tsx
+++ project-hooks/src/components/Home/Home.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Home.module.css";
+import Button from "../UI/Button/Button";
+import AuthContext from "../../store/AuthContext";
 
-const Home: React.FC<{ onLogout: () => void }> = ({ onLogout }) => {
+const Home: React.FC = () => {
+  const { onLogout } = useContext(AuthContext);
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
+      <Button onClick={onLogout}>Logout</Button>
     </Card>
   );
 };
