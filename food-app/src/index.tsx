37e9bf706fd1c6040182c3af30f7368bb708e1aa
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-04 14:23:39 +0300

    feat: add CartContextProvider into app

--- food-app/src/index.tsx
+++ food-app/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { CartContextProvider } from "./store/CartContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </React.StrictMode>
 );
