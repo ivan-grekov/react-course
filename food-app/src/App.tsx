691bb58f82d8c0d846531165b5d8f4a8fd87fb62
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-04 14:23:04 +0300

    feat: add Cart context into App component

--- food-app/src/App.tsx
+++ food-app/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartContext from "./store/CartContext";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  const { isCartIsShown: isCartOpen } = React.useContext(CartContext);
+
+  return (
+    <>
+      {isCartOpen && <Cart />}
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
 }
 
 export default App;
