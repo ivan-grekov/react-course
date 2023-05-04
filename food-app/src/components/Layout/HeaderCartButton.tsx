1920753792c081b7040c487dea6ae1b63a9294d7
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-04 14:21:37 +0300

    feat: add HeaderCartButton component

--- food-app/src/components/Layout/HeaderCartButton.tsx
+++ food-app/src/components/Layout/HeaderCartButton.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/CartContext";
+import { IMealItem } from "../Meals/MealItem/MealItem";
 
 const HeaderCartButton: React.FC = () => {
+  const [btnIshighlighted, setBtnIsHighlighted] = React.useState(false);
+  const { onShowCart, items } = React.useContext(CartContext);
+  const numberOfCartItems = items.reduce((curNumber, item: IMealItem) => {
+    return curNumber + item.amount;
+  }, 0);
+
+  const btnClasses = `${styles.button} ${btnIshighlighted ? styles.bump : ""}`;
+
+  React.useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsHighlighted(true);
+    const btnTimer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(btnTimer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button}>
+    <button className={btnClasses} onClick={onShowCart}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>2</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
