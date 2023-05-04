36e543d979484320e92d3f19473f040371ac8bfd
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-04 14:19:42 +0300

    feat: add MealItemForm component

---
+++ food-app/src/components/Meals/MealItem/MealItemForm.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+
+const MealItemForm: React.FC<{
+  onAddToCart: (amount: number) => void;
+}> = ({ onAddToCart }) => {
+  const amountInputRef = React.useRef<HTMLInputElement>(null);
+  const [amountIsValid, setAmountIsValid] = React.useState(true);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredAmount = amountInputRef.current?.value;
+    const enteredAmountNumber = Number(enteredAmount);
+    if (
+      enteredAmount?.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    onAddToCart(enteredAmountNumber);
+  };
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          idx: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
+    </form>
+  );
+};
+
+export default MealItemForm;
