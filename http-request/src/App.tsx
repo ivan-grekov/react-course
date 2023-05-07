49469eecedeb367d2f64679660c004c9aeb7f9b9
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-07 10:39:48 +0300

    fix: correct component using custom hook useFetchData

--- http-request/src/App.tsx
+++ http-request/src/App.tsx
@@ -1,42 +1,13 @@
 import React from "react";
-import axios, { AxiosError } from "axios";
 import { CircularProgress } from "@mui/material";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 import AddMovie from "./components/AddMovie";
-import { IMovie } from "./components/Movie";
-
-const BASIC_URL: string = "https://swapi.dev/api";
-const FIRE_URL: string =
-  "https://react-http-45a66-default-rtdb.firebaseio.com/movies.json";
+import useFetchData from "./hooks/use-fetchData";
 
 function App() {
-  const [movies, setMovies] = React.useState([]);
-  const [fetchMovies, setFetchMovies] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [error, setIsError] = React.useState<AxiosError>();
-
-  const getMoviesHandler = React.useCallback(async () => {
-    setFetchMovies(true);
-    setIsLoading(true);
-    try {
-      const { data } = await axios.get(`${BASIC_URL}/films`);
-      setMovies(data.results);
-    } catch (err) {
-      console.log(err);
-      setMovies([]);
-      if (err instanceof AxiosError) setIsError(err);
-    }
-    setIsLoading(false);
-  }, []);
-
-  React.useEffect(() => {
-    if (fetchMovies) getMoviesHandler();
-    return () => {
-      setFetchMovies(false);
-    };
-  }, [fetchMovies, getMoviesHandler]);
+  const { movies, error, isLoading, getMoviesHandler, addMovieHandler } =
+    useFetchData();
 
   const content =
     movies.length === 0 && !error ? (
@@ -45,21 +16,6 @@ function App() {
       <MoviesList movies={movies} />
     );
 
-  const addMovieHandler = async (movie: Partial<IMovie>) => {
-    try {
-      const { data } = await axios({
-        method: "POST",
-        url: FIRE_URL,
-        data: JSON.stringify(movie),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
     <React.Fragment>
       <section>
