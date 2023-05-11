cc2995596955f79101dee5e8b818153e3efc3310
Ivan Grekov <ivan_grekov@yahoo.com>
2023-05-11 16:25:16 +0300

    feat: add get meals logic from API

--- food-app/src/components/Meals/AvaliableMeals.tsx
+++ food-app/src/components/Meals/AvaliableMeals.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AvaliableMeals.module.css";
-import { DUMMY_MEALS } from "../../static/dummy-meals";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import useHttp from "../../hooks/use-http";
+export const BASE_URL = "https://react-http-45a66-default-rtdb.firebaseio.com";
 
-interface IMeal {
+export interface IMeal {
   id: string;
   name: string;
   description: string;
@@ -12,7 +13,38 @@ interface IMeal {
 }
 
 const AvaliableMeals: React.FC = () => {
-  const mealsList = DUMMY_MEALS.map((meal: IMeal) => (
+  const [meals, setMeals] = useState<IMeal[] | undefined>([]);
+  const { getData, httpError } = useHttp(`${BASE_URL}/meals.json`);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  useEffect(() => {
+    try {
+      getData().then((data) => {
+        setMeals(data);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.mealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.mealsError}>
+        <p>{httpError.message}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals?.map((meal: IMeal) => (
     <MealItem {...meal} key={meal.id} />
   ));
 
